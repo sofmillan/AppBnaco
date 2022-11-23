5f0730cab4dd8e32fead1f5f9a729517f87ab81a
Sofía Millán <sofiaisabellamillan16@gmail.com>
2022-11-23 11:17:41 -0500

    Third commit

--- app/src/main/java/com/example/bancoapp/DosActivity.java
+++ app/src/main/java/com/example/bancoapp/DosActivity.java
@@ -1,8 +1,11 @@
 package com.example.bancoapp;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,5 +17,15 @@ public class DosActivity extends AppCompatActivity {
         setContentView(R.layout.clientcrud);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        Button btncreateaccount = findViewById(R.id.createaccount);
+
+        btncreateaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DosActivity.this,TresActivity.class);
+                startActivity(i);
+            }
+        });
 }
 }
--- app/src/main/java/com/example/bancoapp/TresActivity.java
+++ app/src/main/java/com/example/bancoapp/TresActivity.java
@@ -1,4 +1,18 @@
 package com.example.bancoapp;
 
-public class TresActivity {
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TresActivity extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cuenta);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
 }
